9a110b7110cdc3bb80ad6bfd0025bd6f04e8a641
정다빈 <dbj01@naver.com>
2024-07-22 18:13:30 +0900

    fix: 폰트 적용 및 불일치하는 경로 수정

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <>
-      <div className="bg-primary font-pretendard">Font 및 색상 테스트</div>
-      <div className="bg-secondary font-pretendard">Font 및 색상 테스트</div>
-      <div className="bg-gray-100 font-pretendard">Font 및 색상 테스트</div>
-      <div className="bg-gray-200 font-pretendard">Font 및 색상 테스트</div>
-      <div className="bg-gray-300 font-pretendard">Font 및 색상 테스트</div>
+      <div className="bg-primary">Font 및 색상 테스트</div>
+      <div className="bg-secondary">Font 및 색상 테스트</div>
+      <div className="bg-gray-100">Font 및 색상 테스트</div>
+      <div className="bg-gray-200">Font 및 색상 테스트</div>
+      <div className="bg-gray-300">Font 및 색상 테스트</div>
     </>
   );
 };
