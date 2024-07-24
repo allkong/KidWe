086af251fdf8eb437ec5fd5fa110ebcfbbad0958
박동환 <znxn123@naver.com>
2024-07-24 11:01:02 +0900

    Revert "feat: MemberRepository 구현 및 테스트 코드 작성"
    This reverts commit 2718017622deb4152adf76214106143159e16d63.

--- back-end/common/src/main/java/yeomeong/common/entity/jpa/member/Member.java
+++ back-end/common/src/main/java/yeomeong/common/entity/jpa/member/Member.java
@@ -3,7 +3,6 @@ package yeomeong.common.entity.jpa.member;
 
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
 import yeomeong.common.entity.jpa.kindergarten.Ban;
@@ -19,7 +18,6 @@ public class Member {
     @Id @GeneratedValue
     private Long id;
 
-    @Email
     private String email;
 
     private String password;
--- back-end/common/src/main/java/yeomeong/common/repository/jpa/MemberRepository.java
+++ back-end/common/src/main/java/yeomeong/common/repository/jpa/MemberRepository.java
@@ -3,7 +3,6 @@ package yeomeong.common.repository.jpa;
 
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
 import yeomeong.common.entity.jpa.member.Member;
 
@@ -20,15 +19,4 @@ public class MemberRepository {
     public Member findOne(Long memberId) {
         return em.find(Member.class , memberId);
     }
-
-    public Member findByEmail(String email) {
-        try {
-            return em.createQuery("SELECT m FROM Member m WHERE m.email = :email", Member.class)
-                    .setParameter("email", email)
-                    .getSingleResult();
-        } catch (EmptyResultDataAccessException e) {
-            throw e;
-        }
-    }
-
 }
