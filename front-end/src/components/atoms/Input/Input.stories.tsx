c1e2dbcd2c2743ea3803ac56459c8ae90dafea02
강혁준 <domohit5124@naver.com>
2024-07-26 09:52:53 +0900

    feat: input 컴포넌트 controlled 및 uncontrolled 동시 지원 구현, 테스트코드 수정

--- front-end/src/components/atoms/Input/Input.stories.tsx
+++ front-end/src/components/atoms/Input/Input.stories.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import {useState} from '@storybook/preview-api';
+import {useState, useRef} from '@storybook/preview-api';
 import type {Meta, StoryObj} from '@storybook/react';
 import Input from '@/components/atoms/Input/Input';
 
@@ -10,15 +9,7 @@ export default meta;
 
 type Story = StoryObj<typeof Input>;
 
-export const Default: Story = {
-  args: {
-    value: 'input',
-    onChange: () => window.alert(),
-    placeholder: 'placeholder test',
-  },
-};
-
-export const State: Story = {
+export const Controlled: Story = {
   args: {},
   render: function () {
     const [text, setText] = useState('');
@@ -27,6 +18,47 @@ export const State: Story = {
       setText(value);
     };
 
-    return <Input value={text} onChange={handleText} />;
+    const handleClick = () => {
+      window.alert(text);
+    };
+
+    return (
+      <div>
+        <Input value={text} onChange={handleText} />
+        <button onClick={handleClick}>[click me]</button>
+        <div>
+          <h1>Re-Rendering Test</h1>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+        </div>
+      </div>
+    );
+  },
+};
+
+export const Uncontrolled: Story = {
+  args: {},
+  render: function () {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleClick = () => {
+      window.alert(inputRef.current?.value);
+    };
+
+    return (
+      <div>
+        <Input ref={inputRef} />
+        <button onClick={handleClick}>[click me]</button>
+        <div>
+          <h1>Re-Rendering Test</h1>
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+        </div>
+      </div>
+    );
   },
 };
