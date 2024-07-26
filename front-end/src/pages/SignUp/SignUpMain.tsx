ea78309101828e04249f3f7446430559baa3fb2a
galler-ist <swghj@naver.com>
2024-07-26 18:47:24 +0900

    design : SignUpMain 변경
    역할 선택 버튼 일부 수정

--- front-end/src/pages/SignUp/SignUpMain.tsx
+++ front-end/src/pages/SignUp/SignUpMain.tsx
@@ -30,7 +30,7 @@ const SignUpMain: React.FC = () => {
           value="원장님"
         />
       </div>
-      <div className="flex items-center justify-center  absolute bottom-8">
+      <div className="flex items-center justify-center px-4 bottom-8 ">
         <Button label="역할 선택" />
       </div>
     </div>
