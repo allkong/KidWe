dc968bf0526b2881f747449e2112f0510242d85d
hyukjun kang <domohit5124@gmail.com>
2024-07-27 16:54:37 +0900

    comment: MemoList 주석 제거

--- front-end/src/pages/kindergarten/MemoList.tsx
+++ front-end/src/pages/kindergarten/MemoList.tsx
@@ -5,11 +5,6 @@ import {useEffect, memo} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {useHeader} from '@/contexts/header/HeaderContext';
 
-/**
- * changeHeaderTitle로 인해 내부 요소가 re-rendering이 되는 것을 방지하기 위해 memo 사용
- * strictMode에서는 의도적으로 컴포넌트가 두번 렌더링되게 함
- * strictMode를 해제하거나 실제 build를 하면 컴포넌트가 한번만 렌더링 됨
- */
 const MemoList = memo(() => {
   const {handleHeaderTitle} = useHeader();
 
