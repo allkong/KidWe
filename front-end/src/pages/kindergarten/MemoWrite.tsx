88697e5f5cb32a967b0852552a493ebe9261592b
강혁준 <domohit5124@naver.com>
2024-07-28 22:40:46 +0900

    feat: 유치원 메모 작성 페이지 구현

--- front-end/src/pages/kindergarten/MemoWrite.tsx
+++ front-end/src/pages/kindergarten/MemoWrite.tsx
@@ -1,12 +1,22 @@
-import WriteButton from '@/components/atoms/Button/WriteButton';
-import {useNavigate} from 'react-router-dom';
+import Button from '@/components/atoms/Button/Button';
+import Divider from '@/components/atoms/Divider/Divider';
+import MemoTimeSelect from '@/components/organisms/Memo/MemoTimeSelect';
+import MemoTagSelect from '@/components/organisms/Memo/MemoTagSelect';
+import KindergartenInfomationSelect from '@/components/organisms/Memo/KindergartenInfomationSelect';
 
 const MemoWrite = () => {
-  const navigate = useNavigate();
-
   return (
-    <div>
-      <WriteButton onClick={() => navigate('/kindergarten/memo')}></WriteButton>
+    <div className="flex flex-col h-full bg-white">
+      <div className="flex-grow px-5 py-5 space-y-6 overflow-y-scroll">
+        <MemoTimeSelect />
+        <Divider />
+        <MemoTagSelect />
+        <Divider />
+        <KindergartenInfomationSelect />
+      </div>
+      <div className="px-5 py-6 h-fit min-h-fit min-w-fit">
+        <Button label="메모 작성하기" />
+      </div>
     </div>
   );
 };
