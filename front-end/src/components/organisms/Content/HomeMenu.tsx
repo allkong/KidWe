ce694f999f9f62153e291aceea3c253399ea9302
정다빈 <dbj01@naver.com>
2024-07-30 11:40:08 +0900

    feat: 홈 메뉴 링크 연결

--- front-end/src/components/organisms/Content/HomeMenu.tsx
+++ front-end/src/components/organisms/Content/HomeMenu.tsx
@@ -1,17 +1,18 @@
+import {Link} from 'react-router-dom';
 import MainMenu from '@/components/atoms/Menu/MainMenu';
 
 const menuItems = [
-  {img: 'sketchBook', text: '알림장'},
-  {img: 'megaphone', text: '공지사항'},
-  {img: 'attendance', text: '출석부'},
-  {img: 'book', text: '메모'},
-  {img: 'riceBowl', text: '식단'},
-  {img: 'medication', text: '투약의뢰서'},
-  {img: 'house', text: '귀가동의서'},
-  {img: 'bus', text: '스쿨버스'},
-  {img: 'folder', text: '일지'},
-  {img: 'imageGallery', text: '앨범'},
-  {img: 'dinosaur', text: '반 관리'},
+  {img: 'sketchBook', text: '알림장', to: ''},
+  {img: 'megaphone', text: '공지사항', to: ''},
+  {img: 'attendance', text: '출석부', to: ''},
+  {img: 'book', text: '메모', to: ''},
+  {img: 'riceBowl', text: '식단', to: ''},
+  {img: 'medication', text: '투약의뢰서', to: ''},
+  {img: 'house', text: '귀가동의서', to: ''},
+  {img: 'bus', text: '스쿨버스', to: ''},
+  {img: 'folder', text: '일지', to: ''},
+  {img: 'imageGallery', text: '앨범', to: ''},
+  {img: 'dinosaur', text: '반 관리', to: ''},
 ];
 
 const HomeMenu = () => {
@@ -20,7 +21,9 @@ const HomeMenu = () => {
       <div className="grid grid-cols-4 p-7 gap-x-7 gap-y-5">
         {/* 역할에 따라 보여주는 메뉴가 달라야 함 */}
         {menuItems.map((item, index) => (
-          <MainMenu key={index} img={item.img} text={item.text} />
+          <Link to={item.to}>
+            <MainMenu key={index} img={item.img} text={item.text} />
+          </Link>
         ))}
       </div>
     </div>
