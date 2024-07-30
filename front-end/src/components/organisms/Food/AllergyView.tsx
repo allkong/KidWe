81cf8281b73ce853170c1b13c218ded5cc181694
강혁준 <domohit5124@naver.com>
2024-07-30 22:15:22 +0900

    fix: 급식 및 영양정보 등록 페이지 모달 벗어날 시 알레르기 정보 사라지는 오류 해결

--- front-end/src/components/organisms/Food/AllergyView.tsx
+++ front-end/src/components/organisms/Food/AllergyView.tsx
@@ -1,12 +1,36 @@
+import type {Allergy} from '@/constants/allergy';
+import CheckBoxButton from '@/components/atoms/CheckBox/CheckBoxButton';
+
 interface AllergyViewProps {
-  render?: () => React.ReactNode;
+  datas?: Allergy[];
+  onChangeData?: (value: Allergy[]) => void;
 }
 
-const AllergyView = ({render}: AllergyViewProps) => {
+const AllergyView = ({datas, onChangeData}: AllergyViewProps) => {
+  const handleCheckBoxClick = (id: number) => {
+    if (datas !== undefined) {
+      onChangeData?.(
+        datas.map(data =>
+          data.id === id ? {...data, isChecked: !data.isChecked} : {...data}
+        )
+      );
+    }
+  };
+
   return (
     <div className="flex items-center justify-center w-full h-full py-6">
       <div className="grid h-full grid-flow-row grid-cols-3 gap-3 w-fit">
-        {render?.()}
+        {datas &&
+          datas.map(data => (
+            <CheckBoxButton
+              key={data.id}
+              label={data.value}
+              onClick={() => {
+                handleCheckBoxClick(data.id);
+              }}
+              isChecked={data.isChecked}
+            />
+          ))}
       </div>
     </div>
   );
---
+++ front-end/src/constants/allergy.ts
@@ -0,0 +1,68 @@
+export interface Allergy {
+  id: number;
+  value: string;
+  isChecked: boolean;
+}
+
+export const allergies: Allergy[] = [
+  {
+    id: 1,
+    value: '달걀',
+    isChecked: false,
+  },
+  {
+    id: 2,
+    value: '우유',
+    isChecked: false,
+  },
+  {
+    id: 3,
+    value: '메밀',
+    isChecked: false,
+  },
+  {
+    id: 4,
+    value: '땅콩',
+    isChecked: false,
+  },
+  {
+    id: 5,
+    value: '대두',
+    isChecked: false,
+  },
+  {
+    id: 6,
+    value: '밀',
+    isChecked: false,
+  },
+  {
+    id: 7,
+    value: '고등어',
+    isChecked: false,
+  },
+  {
+    id: 8,
+    value: '게',
+    isChecked: false,
+  },
+  {
+    id: 9,
+    value: '돼지고기',
+    isChecked: false,
+  },
+  {
+    id: 10,
+    value: '새우',
+    isChecked: false,
+  },
+  {
+    id: 11,
+    value: '복숭아',
+    isChecked: false,
+  },
+  {
+    id: 12,
+    value: '토마토',
+    isChecked: false,
+  },
+];
