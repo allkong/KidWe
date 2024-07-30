789f3c2c714eba102ba8ab593539c642156734d7
강혁준 <domohit5124@naver.com>
2024-07-30 14:31:29 +0900

    feat: InputForm Uncontrolled 구현 및 테스트코드 수정

--- front-end/src/components/molecules/InputForm/InputForm.stories.tsx
+++ front-end/src/components/molecules/InputForm/InputForm.stories.tsx
@@ -1,5 +1,6 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import InputForm from '@/components/molecules/InputForm/InputForm';
+import {useRef, useState} from 'storybook/internal/preview-api';
 
 const meta: Meta<typeof InputForm> = {
   component: InputForm,
@@ -9,9 +10,30 @@ export default meta;
 type Story = StoryObj<typeof InputForm>;
 
 export const Default: Story = {
-  args: {
-    value: 'input',
-    placeholder: 'placeholder',
-    label: '입력',
+  render: function () {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    return (
+      <InputForm
+        buttonLabel="버튼"
+        onClick={() => window.alert(inputRef.current?.value)}
+        ref={inputRef}
+      />
+    );
+  },
+};
+
+export const State: Story = {
+  render: function () {
+    const [value, setValue] = useState('');
+
+    return (
+      <InputForm
+        buttonLabel="버튼"
+        inputValue={value}
+        setValue={value => setValue(value)}
+        onClick={() => window.alert(value)}
+      />
+    );
   },
 };
