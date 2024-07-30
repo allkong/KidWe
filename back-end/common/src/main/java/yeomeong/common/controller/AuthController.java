bd29bce8b22622a3b3958c00d9fb64a895ac1466
박동환 <znxn123@naver.com>
2024-07-30 16:02:19 +0900

    feat: auth, member domain - swagger tag, operation 추가

--- back-end/common/src/main/java/yeomeong/common/controller/AuthController.java
+++ back-end/common/src/main/java/yeomeong/common/controller/AuthController.java
@@ -1,5 +1,7 @@
 package yeomeong.common.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +15,7 @@ import yeomeong.common.security.jwt.JwtUtil;
 import yeomeong.common.service.MemberService;
 
 @RestController
+@Tag(name = "인증/인가 API", description = "인증/인가 관련 API")
 public class AuthController {
 
     final MemberService memberService;
@@ -23,12 +26,14 @@ public class AuthController {
         this.jwtService = jwtService;
     }
 
+    @Operation(summary = "회원가입", description = "회원가입을 합니다.")
     @PostMapping("/join")
     public ResponseEntity<Void> join(@RequestBody JoinRequestDto joinRequestDto) {
         memberService.joinMember(joinRequestDto);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @Operation(summary = "Access JWT 재요청", description = "Access JWT를 재요청합니다.")
     @PostMapping("/refresh")
     public ResponseEntity<?> refresh(@RequestHeader("Authorization") String refreshToken) {
         if (jwtService.isTokenStored(refreshToken)) {
@@ -41,4 +46,5 @@ public class AuthController {
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
+
 }
--- back-end/common/src/main/java/yeomeong/common/controller/MemberController.java
+++ back-end/common/src/main/java/yeomeong/common/controller/MemberController.java
@@ -1,13 +1,18 @@
 package yeomeong.common.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import yeomeong.common.dto.member.MemberProfileResponseDto;
 import yeomeong.common.service.MemberService;
 
 @Slf4j
 @RestController("/member")
+@Tag(name = "사용자 API", description = "사용자 관련 API")
 public class MemberController {
 
     final MemberService memberService;
@@ -16,15 +21,17 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @Operation(summary = "사용자 조회", description = "특정 사용자 정보를 조회합니다.")
     @GetMapping("/profile")
-    public ResponseEntity<?> getMemberProfile(Authentication authentication) {
-        return ResponseEntity.ok(memberService.getMemberProfile(authentication.getName()));
+    public ResponseEntity<MemberProfileResponseDto> getMemberProfile(Authentication authentication) {
+        return ResponseEntity.status(HttpStatus.OK).body(memberService.getMemberProfile(authentication.getName()));
     }
 
+    @Operation(summary = "사용자 삭제", description = "특정 사용자를 삭제합니다.")
     @DeleteMapping("/profile")
-    public ResponseEntity<?> updateMemberProfile(Authentication authentication) {
+    public ResponseEntity<Void> updateMemberProfile(Authentication authentication) {
         memberService.deleteMember(authentication.getName());
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 }
