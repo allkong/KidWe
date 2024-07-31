8c92afdcac39c806182d7ce228b6431c62145c41
박동환 <znxn123@naver.com>
2024-07-31 13:27:44 +0900

    feat: kindergarten read API 구현

--- back-end/common/src/main/java/yeomeong/common/controller/KindergartenController.java
+++ back-end/common/src/main/java/yeomeong/common/controller/KindergartenController.java
@@ -2,14 +2,20 @@ package yeomeong.common.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import yeomeong.common.dto.kindergarten.KindergartenInfoResponseDto;
 import yeomeong.common.dto.kindergarten.KindergartenSaveRequestDto;
 import yeomeong.common.service.KindergartenService;
 
 @RestController
+@RequestMapping("/kindergartens")
 @Tag(name = "유치원 API", description = "유치원 관련 API")
 public class KindergartenController {
 
@@ -20,10 +26,16 @@ public class KindergartenController {
     }
 
     @Operation(summary = "유치원 생성", description = "유치원을 생성합니다.")
-    @PostMapping("/kindergarten")
+    @PostMapping
     public ResponseEntity<Void> createKindergarten(@RequestBody KindergartenSaveRequestDto kindergartenSaveRequestDto) {
         kindergartenService.createKindergarten(kindergartenSaveRequestDto);
         return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "유치원 조회", description = "특정 유치원을 조회합니다.")
+    @GetMapping("/{kindergartenId}")
+    public ResponseEntity<KindergartenInfoResponseDto> getKindergarten(@PathVariable Long kindergartenId) {
+        return ResponseEntity.status(HttpStatus.OK).body(kindergartenService.getKindergartenInfo(kindergartenId));
     }
 
 }
--- back-end/common/src/main/java/yeomeong/common/service/KindergartenService.java
+++ back-end/common/src/main/java/yeomeong/common/service/KindergartenService.java
@@ -1,10 +1,16 @@
 package yeomeong.common.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import yeomeong.common.dto.kindergarten.KindergartenInfoResponseDto;
 import yeomeong.common.dto.kindergarten.KindergartenSaveRequestDto;
+import yeomeong.common.entity.kindergarten.Kindergarten;
+import yeomeong.common.exception.CustomException;
+import yeomeong.common.exception.ErrorCode;
 import yeomeong.common.repository.KindergartenRepository;
 
 @Service
+@Slf4j
 public class KindergartenService {
 
     final KindergartenRepository kindergartenRepository;
@@ -15,6 +21,16 @@ public class KindergartenService {
 
     public void createKindergarten(KindergartenSaveRequestDto kindergartenSaveRequestDto) {
         kindergartenRepository.save(KindergartenSaveRequestDto.toKindergartenEntity(kindergartenSaveRequestDto));
+    }
+
+    public KindergartenInfoResponseDto getKindergartenInfo(Long kindergartenId) {
+        Kindergarten kindergarten = kindergartenRepository.findById(kindergartenId)
+            .orElseThrow(() -> new CustomException(ErrorCode.INVALID_ID));
+        log.info("KindergartenId: {}", kindergarten.getId());
+
+        KindergartenInfoResponseDto kindergartenInfoResponseDto = KindergartenInfoResponseDto.toKindergartenDto(kindergarten);
+        log.info("Dto Info: {}", kindergartenInfoResponseDto.toString());
+        return kindergartenInfoResponseDto;
     }
 
 }
