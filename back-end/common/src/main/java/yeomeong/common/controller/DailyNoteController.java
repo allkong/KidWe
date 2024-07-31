a2201e53b514161cbd39e9f1120a64f41263791c
sjmrabbit <sjmrabbit@naver.com>
2024-07-31 11:27:42 +0900

    Feat : DailyNoteController, Service 구현(ver1) 완료

--- back-end/common/src/main/java/yeomeong/common/controller/DailyNoteController.java
+++ back-end/common/src/main/java/yeomeong/common/controller/DailyNoteController.java
@@ -3,9 +3,11 @@ package yeomeong.common.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,7 @@ import yeomeong.common.service.DailyNoteService;
 @RequestMapping("/dailynote")
 @Tag(name = "알림장", description = "알림장 관련 API")
 public class DailyNoteController {
+
     private final DailyNoteService dailyNoteService;
 
     @PostMapping("/")
@@ -27,8 +30,25 @@ public class DailyNoteController {
 
     @GetMapping("/{kid_id}/{year}/{month}")
     public List<DailyNote> getDailyNotes(@PathVariable("kid_id") Long kidId,
-                                            @PathVariable("year") String year,
-                                            @PathVariable("year") String month){
-        return null;
+        @PathVariable("year") String year,
+        @PathVariable("year") String month) {
+        String yearAndMonth = year + "-" + month;
+        return dailyNoteService.getDailyNotes(kidId, yearAndMonth);
+    }
+
+    @GetMapping("/{dailynote_id}")
+    public DailyNote getDailyNote(@PathVariable("dailynote_id") Long id) {
+        return dailyNoteService.getDailyNote(id);
+    }
+
+    @PutMapping("/{dailynote_id")
+    public DailyNote updateDailyNote(@PathVariable("dailynote_id") Long id,
+        @RequestBody DailyNote dailyNote) {
+        return dailyNoteService.updateDailyNote(id, dailyNote);
+    }
+
+    @DeleteMapping("/{dailynote_id}")
+    public void deleteDailyNote(@PathVariable("dailynote_id") Long id) {
+        dailyNoteService.deleteDailyNote(id);
     }
 }
--- back-end/common/src/main/java/yeomeong/common/service/DailyNoteService.java
+++ back-end/common/src/main/java/yeomeong/common/service/DailyNoteService.java
@@ -1,6 +1,5 @@
 package yeomeong.common.service;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -11,28 +10,47 @@ import yeomeong.common.repository.DailyNoteRepository;
 @Service
 @RequiredArgsConstructor
 public class DailyNoteService {
+
     private final DailyNoteRepository dailyNoteRepository;
+
     //알림장 생성하기
     public DailyNote createDailyNote(DailyNote dailyNote) {
         return dailyNoteRepository.save(dailyNote);
     }
-    
+
     //날짜별&아이별 알림장 조회하기
     public List<DailyNote> getDailyNotes(Long kidId, String yearAndMonth) {
         return dailyNoteRepository.findByKidIdAndYearAndMonth(kidId, yearAndMonth);
     }
-    
+
+    //알림장 조회하기
+    public DailyNote getDailyNote(Long id) {
+        return dailyNoteRepository.findById(id).orElse(null);
+    }
+
     //알림장 수정하기
-    public DailyNote updateDailyNote(DailyNote updatedDailyNote) {
-        Optional<DailyNote> isExistDailyNote = dailyNoteRepository.findById(updatedDailyNote.getId());
+    public DailyNote updateDailyNote(Long id, DailyNote updatedDailyNote) {
+        Optional<DailyNote> isExistDailyNote = dailyNoteRepository.findById(
+            updatedDailyNote.getId());
         DailyNote oldDailyNote;
-        if(isExistDailyNote.isPresent()) {
+        if (isExistDailyNote.isPresent()) {
             oldDailyNote = isExistDailyNote.get();
             // 업데이트 하기
             return dailyNoteRepository.save(oldDailyNote);
         }
         return null;
     }
-    
+
     //알림장 삭제하기
+    public boolean deleteDailyNote(Long id) {
+        Optional<DailyNote> isExistDailyNote = dailyNoteRepository.findById(id);
+        DailyNote oldDailyNote;
+        if (isExistDailyNote.isPresent()) {
+            oldDailyNote = isExistDailyNote.get();
+            oldDailyNote.setIsDeleted(true);
+            dailyNoteRepository.save(oldDailyNote);
+            return true;
+        }
+        return false;
+    }
 }
