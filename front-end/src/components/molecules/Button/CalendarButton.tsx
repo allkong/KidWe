5d49c99f3e18999e7d27bbfa3a0ed26797e3f278
강혁준 <domohit5124@naver.com>
2024-08-02 17:38:49 +0900

    style: CalendarButton 스타일 및 props 변경

--- front-end/src/components/molecules/Button/CalendarButton.tsx
+++ front-end/src/components/molecules/Button/CalendarButton.tsx
@@ -5,9 +5,22 @@ import dayjs, {Dayjs} from 'dayjs';
 interface CalendarButtonProps {
   render: () => React.ReactNode;
   onClick?: (value: Dayjs) => void;
+  left?: boolean;
+  right?: boolean;
+  defaultDate?: Dayjs;
+  showNavigation?: boolean;
+  showNeighboringMonth?: boolean;
 }
 
-const CalendarButton = ({render, onClick}: CalendarButtonProps) => {
+const CalendarButton = ({
+  render,
+  onClick,
+  left,
+  right,
+  defaultDate,
+  showNavigation,
+  showNeighboringMonth,
+}: CalendarButtonProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const selectRef = useRef<HTMLDivElement>(null);
@@ -32,14 +45,24 @@ const CalendarButton = ({render, onClick}: CalendarButtonProps) => {
     onClick?.(dayjs(value));
   };
 
+  const leftClass = left ? '-right-3' : '';
+  const rightClass = right ? '-left-3' : '';
+
   return (
     <div ref={selectRef} className="relative w-fit" onClick={handleOnClick}>
       {render()}
       {isOpen && (
         <>
           <div className="absolute w-0 h-0 border-b-8 border-l-8 border-r-8 border-white shadow-lg top-10 border-r-transparent border-l-transparent"></div>
-          <div className="absolute px-2 py-2 bg-white rounded-lg -right-3 top-[36px] w-72 shadow-lg">
-            <CustomCalendar onChange={handleChange} />
+          <div
+            className={`absolute px-2 py-2 bg-white rounded-lg ${leftClass} ${rightClass} top-[36px] w-72 shadow-lg`}
+          >
+            <CustomCalendar
+              onChange={handleChange}
+              defaultDate={defaultDate?.toDate()}
+              showNavigation={showNavigation}
+              showNeighboringMonth={showNeighboringMonth}
+            />
           </div>
         </>
       )}
