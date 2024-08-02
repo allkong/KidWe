328b0504dcdf7846739dfc14f19d35e46d29b14b
강혁준 <domohit5124@naver.com>
2024-08-02 17:42:25 +0900

    feat: 메모 정보 atom 추가 및 요청 구현

--- front-end/src/pages/kindergarten/MemoWrite.tsx
+++ front-end/src/pages/kindergarten/MemoWrite.tsx
@@ -3,10 +3,30 @@ import Divider from '@/components/atoms/Divider/Divider';
 import MemoTimeSelect from '@/components/organisms/Memo/MemoTimeSelect';
 import MemoTagSelect from '@/components/organisms/Memo/MemoTagSelect';
 import KindergartenInfomationSelect from '@/components/organisms/Memo/KindergartenInfomationSelect';
+import Header from '@/components/organisms/Navigation/Header';
+import {containerHeaderClass} from '@/styles/styles';
+import {writeMemo} from '@/apis/memo/writeMemo';
+import {useMutation} from '@tanstack/react-query';
+import {useRecoilValue} from 'recoil';
+import {memoState} from '@/recoil/atoms/memo/memo';
 
 const MemoWrite = () => {
+  const teacherId = 1;
+  const memo = useRecoilValue(memoState);
+
+  const writeMutate = useMutation({
+    mutationFn: () => {
+      return writeMemo(teacherId, memo);
+    },
+  });
+
+  const handleClick = () => {
+    writeMutate.mutate();
+  };
+
   return (
-    <div className="flex flex-col h-full bg-white">
+    <div className={`${containerHeaderClass} flex flex-col h-full bg-white`}>
+      <Header title="관찰 메모 작성" buttonType="close" />
       <div className="flex-grow px-5 py-5 space-y-6 overflow-y-scroll">
         <MemoTimeSelect />
         <Divider />
@@ -15,7 +35,7 @@ const MemoWrite = () => {
         <KindergartenInfomationSelect />
       </div>
       <div className="px-5 py-6 h-fit min-h-fit min-w-fit">
-        <Button label="메모 작성하기" onClick={() => {}} size="large" />
+        <Button label="메모 작성하기" onClick={handleClick} size="large" />
       </div>
     </div>
   );
---
+++ front-end/src/recoil/atoms/memo/memo.ts
@@ -0,0 +1,27 @@
+import {atom} from 'recoil';
+import type {Tag} from '@/types/memo/Tag';
+import dayjs from 'dayjs';
+
+export interface Kid {
+  id: number;
+  name: string;
+}
+
+export interface Memo {
+  updatedTime: string;
+  lesson: string;
+  kids: Kid[];
+  tagRequestDtos: Tag[];
+  content: string;
+}
+
+export const memoState = atom<Memo>({
+  key: 'memo',
+  default: {
+    updatedTime: dayjs().toString(),
+    lesson: '',
+    kids: [],
+    tagRequestDtos: [],
+    content: '',
+  },
+});
