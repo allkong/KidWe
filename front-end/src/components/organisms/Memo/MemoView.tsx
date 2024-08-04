dc0365688a7e50ac6a290991dcd65953257b00b0
강혁준 <domohit5124@naver.com>
2024-08-04 23:50:08 +0900

    style: 관찰 메모에 병아리 추가

--- front-end/src/components/organisms/Memo/MemoView.tsx
+++ front-end/src/components/organisms/Memo/MemoView.tsx
@@ -1,4 +1,5 @@
 import Tag from '@/components/atoms/Tag/Tag';
+import chick from '@/assets/icons/chick-1.svg';
 
 interface MemoViewProps {
   time?: string;
@@ -12,7 +13,10 @@ const MemoView = ({time, lesson, children, tags, content}: MemoViewProps) => {
   return (
     <div className="flex flex-col items-start justify-center w-full h-full px-3 py-3 space-y-3 text-gray-300">
       <div>
-        <p>{time}</p>
+        <div className="flex items-center gap-2">
+          <img src={chick} />
+          <p>{time}</p>
+        </div>
         <p className="text-xl font-semibold">{lesson}</p>
         <div className="flex flex-wrap items-end gap-1">
           <p className="text-sm font-semibold">{children?.join(', ')}</p>
