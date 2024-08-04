b8f11857f5e43cacaaa47d7bbb3aded3bd516076
ParkDH0809 <znxn1237@gmail.com>
2024-08-04 21:16:33 +0900

    feat: 반 이름 변경 API 구현

--- back-end/common/src/main/java/yeomeong/common/controller/BanController.java
+++ back-end/common/src/main/java/yeomeong/common/controller/BanController.java
@@ -7,11 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yeomeong.common.dto.ban.BanCreateRequestDto;
 import yeomeong.common.dto.ban.BanDetailInfoDto;
+import yeomeong.common.dto.ban.BanNameChangeRequestDto;
 import yeomeong.common.service.BanService;
 
 @RestController
@@ -27,8 +29,7 @@ public class BanController {
 
     @Operation(summary = "반 생성", description = "반을 생성합니다.")
     @PostMapping
-    public ResponseEntity<Void> createBan(
-        @RequestBody BanCreateRequestDto banCreateRequestDto) {
+    public ResponseEntity<Void> createBan(@RequestBody BanCreateRequestDto banCreateRequestDto) {
         banService.createBan(banCreateRequestDto);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
@@ -37,6 +38,13 @@ public class BanController {
     @GetMapping("{banId}")
     public ResponseEntity<BanDetailInfoDto> getBanInfo(@PathVariable Long banId) {
         return ResponseEntity.status(HttpStatus.OK).body(banService.getBanInfo(banId));
+    }
+
+    @Operation(summary = "반 이름 변경", description = "특정 반 이름을 변경합니다.")
+    @PutMapping
+    public ResponseEntity<Void> changeBanName(@RequestBody BanNameChangeRequestDto banNameChangeRequestDto) {
+        banService.changeBanName(banNameChangeRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 }
--- back-end/common/src/main/java/yeomeong/common/service/BanService.java
+++ back-end/common/src/main/java/yeomeong/common/service/BanService.java
@@ -3,6 +3,7 @@ package yeomeong.common.service;
 import org.springframework.stereotype.Service;
 import yeomeong.common.dto.ban.BanCreateRequestDto;
 import yeomeong.common.dto.ban.BanDetailInfoDto;
+import yeomeong.common.dto.ban.BanNameChangeRequestDto;
 import yeomeong.common.exception.CustomException;
 import yeomeong.common.exception.ErrorCode;
 import yeomeong.common.repository.BanRepository;
@@ -30,6 +31,12 @@ public class BanService {
     public BanDetailInfoDto getBanInfo(Long banId) {
         return BanDetailInfoDto.toBanDetailInfoDto(
             banRepository.findById(banId).orElseThrow(() -> new CustomException(ErrorCode.INVALID_ID)));
+    }
+
+    public void changeBanName(BanNameChangeRequestDto banNameChangeRequestDto) {
+        if (banRepository.changeBanName(banNameChangeRequestDto.getId(), banNameChangeRequestDto.getName()) != 1) {
+            throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
+        }
     }
 
 }
