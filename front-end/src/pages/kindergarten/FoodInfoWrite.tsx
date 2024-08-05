1e45fc32a016fd3ad7228b519bc38617cb84357e
강혁준 <domohit5124@naver.com>
2024-08-05 17:49:02 +0900

    feat: DateNavigator 기능 구현

--- front-end/src/pages/kindergarten/FoodInfoWrite.tsx
+++ front-end/src/pages/kindergarten/FoodInfoWrite.tsx
@@ -3,13 +3,17 @@ import FoodInfoWriteItem from '@/components/organisms/Food/FoodInfoWriteItem';
 import Header from '@/components/organisms/Navigation/Header';
 import NavigationBar from '@/components/organisms/Navigation/NavigationBar';
 import {containerHeaderClass} from '@/styles/styles';
+import {useLocation} from 'react-router-dom';
 
 const FoodInfoWrite = () => {
+  const location = useLocation();
+  const {date} = location.state;
+
   return (
     <div className={`${containerHeaderClass} flex flex-col h-full`}>
       <Header title="메뉴 정보 등록" buttonType="back" />
       <div className="flex justify-end px-5 py-6 text-xs h-fit min-h-fit min-w-fit">
-        날짜 선택
+        <p>{date}</p>
       </div>
       <div className="flex-grow px-5 py-5 space-y-6 overflow-y-scroll">
         <FoodInfoWriteItem label="중식" />
