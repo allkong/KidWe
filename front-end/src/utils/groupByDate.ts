d587dac939251a70571e4d4935388024c8358b7c
정다빈 <dbj01@naver.com>
2024-08-05 19:31:26 +0900

    feat: 투약의뢰서 목록 API 연동 - 학부모, 선생님 시점 목록 api 연동 - MedicationItem type 분리 - date navigator 월별 이동 구현 - react query, axios로 데이터 요청

--- front-end/src/utils/groupByDate.ts
+++ front-end/src/utils/groupByDate.ts
@@ -1,20 +1,14 @@
 import dayjs from 'dayjs';
-
-interface MedicationData {
-  medicationId: number;
-  kidName: string;
-  banName: string;
-  kidDate: string;
-}
+import type {MedicationItem} from '@/types/medication/MedicationList';
 
 interface GroupedData {
-  [key: string]: MedicationData[];
+  [key: string]: MedicationItem[];
 }
 
-export const groupByDate = (data: MedicationData[]): GroupedData => {
+export const groupByDate = (data: MedicationItem[]): GroupedData => {
   // 데이터를 날짜별로 그룹화
   const grouped = data.reduce((acc: GroupedData, item) => {
-    const date = dayjs(item.kidDate).format('YYYY-MM-DD');
+    const date = dayjs(item.medicationCreatedDateTime).format('YYYY-MM-DD');
     if (!acc[date]) {
       acc[date] = [];
     }
@@ -22,11 +16,17 @@ export const groupByDate = (data: MedicationData[]): GroupedData => {
     return acc;
   }, {});
 
-  // 그룹화된 데이터를 날짜 최신순으로 정렬
+  // 그룹화된 데이터를 날짜 최신순으로 정렬하고, 각 그룹 내에서도 시간 최신순으로 정렬
   const sortedGroupedData = Object.keys(grouped)
     .sort((a, b) => (dayjs(b).isAfter(dayjs(a)) ? 1 : -1))
     .reduce((acc: GroupedData, key) => {
-      acc[key] = grouped[key];
+      acc[key] = grouped[key].sort((a, b) =>
+        dayjs(b.medicationCreatedDateTime).isAfter(
+          dayjs(a.medicationCreatedDateTime)
+        )
+          ? 1
+          : -1
+      );
       return acc;
     }, {});
 
