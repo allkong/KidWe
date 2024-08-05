ea613d351c3b5b162ed20777311c909e12d7c228
sjmrabbit <sjmrabbit@naver.com>
2024-08-05 14:35:35 +0900

    Feat : Add Controller Operation

--- back-end/common/src/main/java/yeomeong/common/controller/DailyNoteCommentController.java
+++ back-end/common/src/main/java/yeomeong/common/controller/DailyNoteCommentController.java
@@ -1,5 +1,6 @@
 package yeomeong.common.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 import yeomeong.common.dto.post.dailynote.request.DailyNoteCommentCreateRequestDto;
 import yeomeong.common.dto.post.dailynote.request.DailyNoteCommentUpdateRequestDto;
 import yeomeong.common.dto.post.dailynote.response.DailyNoteCommentResponseDto;
-import yeomeong.common.entity.post.comment.DailyNoteComment;
 import yeomeong.common.service.DailyNoteCommentService;
 
 @RequiredArgsConstructor
@@ -24,18 +24,21 @@ import yeomeong.common.service.DailyNoteCommentService;
 public class DailyNoteCommentController {
     private final DailyNoteCommentService dailyNoteCommentService;
 
+    @Operation(summary = "알림장 (대)댓글 생성 API", description = "알림장 (대)댓글을 생성합니다. (댓글인 경우 parent_comment를 null로, 대댓글인 경우 댓글의 id를 담아 보내주세요)")
     @PostMapping("/")
     ResponseEntity<DailyNoteCommentResponseDto> createDailyNoteComment(@RequestBody DailyNoteCommentCreateRequestDto dailyNoteCommentCreateRequestDto) {
         DailyNoteCommentResponseDto createdDailyNoteComment = dailyNoteCommentService.createDailyNoteComment(dailyNoteCommentCreateRequestDto);
         return ResponseEntity.ok(createdDailyNoteComment);
     }
 
+    @Operation(summary = "알림장 (대)댓글 수정 API", description = "알림장 (대)댓글을 수정합니다.")
     @PutMapping("/")
     ResponseEntity<DailyNoteCommentResponseDto> updateDailyNoteComment(@RequestBody DailyNoteCommentUpdateRequestDto dailyNoteCommentUpdateRequestDto) {
         DailyNoteCommentResponseDto updatedDailyNoteComment = dailyNoteCommentService.updateDailyNoteComment(dailyNoteCommentUpdateRequestDto);
         return ResponseEntity.ok(updatedDailyNoteComment);
     }
 
+    @Operation(summary = "알림장 (대)댓글 삭제 API", description = "알림장 (대)댓글을 삭제합니다.")
     @DeleteMapping("/{daily_note_comment_id}")
     ResponseEntity<String> deleteDailyNoteComment(@PathVariable("daily_note_comment_id") Long dailyNoteCommentId) {
         dailyNoteCommentService.deleteDailyNoteComment(dailyNoteCommentId);
--- back-end/common/src/main/java/yeomeong/common/controller/DailyNoteController.java
+++ back-end/common/src/main/java/yeomeong/common/controller/DailyNoteController.java
@@ -1,5 +1,6 @@
 package yeomeong.common.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +27,7 @@ public class DailyNoteController {
 
     private final DailyNoteService dailyNoteService;
 
+    @Operation(summary = "알림장 생성 API", description = "알림장을 생성합니다.")
     @PostMapping("/")
     public ResponseEntity<DailyNoteResponseDto> createDailyNote(@RequestBody DailyNoteCreateRequestDto dailyNoteCreateRequestDto) {
         DailyNoteResponseDto createdDailyNote = dailyNoteService.createDailyNote(dailyNoteCreateRequestDto);
@@ -33,6 +35,7 @@ public class DailyNoteController {
         return ResponseEntity.ok(createdDailyNote);
     }
 
+    @Operation(summary = "월별 알림장 조회 API", description = "월별로 알림장을 조회해 List로 반환합니다.")
     @GetMapping("/{member_id}/{kid_id}/{year}/{month}")
     public ResponseEntity<List<DailyNoteResponseDto>> getDailyNotes(@PathVariable("member_id") Long memberId,
         @PathVariable("kid_id") Long kidId,
@@ -42,16 +45,19 @@ public class DailyNoteController {
         return ResponseEntity.ok(dailyNoteService.getDailyNotes(memberId, kidId, yearAndMonth));
     }
 
+    @Operation(summary = "특정 알림장 상세정보 조회 API", description = "특정 알림장의 상세정보를 반환합니다.")
     @GetMapping("/{dailynote_id}")
     public ResponseEntity<DailyNoteResponseDto> getDailyNote(@PathVariable("dailynote_id") Long id) {
         return ResponseEntity.ok(dailyNoteService.getDailyNote(id));
     }
 
+    @Operation(summary = "알림장 수정 API", description = "전송시간이 지나지 않은 알림장에 한정해 알림장을 수정합니다.")
     @PutMapping("/")
     public ResponseEntity<DailyNoteResponseDto> updateDailyNote(@RequestBody DailyNoteUpdateRequestDto dailyNoteRequestDto) {
         return ResponseEntity.ok(dailyNoteService.updateDailyNote(dailyNoteRequestDto));
     }
 
+    @Operation(summary = "알림장 삭제 API", description = "알림장을 삭제합니다.")
     @DeleteMapping("/{dailynote_id}")
     public ResponseEntity<Void> deleteDailyNote(@PathVariable("dailynote_id") Long id) {
         dailyNoteService.deleteDailyNote(id);
