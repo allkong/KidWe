dad01cfc2d7e2f87a2002a4562c6f975017f75d3
박동환 <znxn123@naver.com>
2024-08-05 10:49:24 +0900

    feat: security CORS config 추가

---
+++ back-end/common/src/main/java/yeomeong/common/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package yeomeong.common.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Controller
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*") // “*“같은 와일드카드를 사용
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE") // 허용할 HTTP method
+            .allowCredentials(true);
+    }
+
+}
