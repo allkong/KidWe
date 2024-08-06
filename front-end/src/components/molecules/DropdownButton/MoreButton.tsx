311e83edf3e2f681405e5dc4439f8ae85e54da9e
강혁준 <domohit5124@naver.com>
2024-08-06 13:05:44 +0900

    feat: MoreButton 컴포넌트 구현

--- front-end/src/components/molecules/DropdownButton/MoreButton.tsx
+++ front-end/src/components/molecules/DropdownButton/MoreButton.tsx
@@ -1,53 +1,8 @@
-import Icon from '@/assets/icons/more-line.svg';
-import Dropdown from '@/components/atoms/Dropdown/Dropdown';
-import {useEffect, useRef, useState} from 'react';
+import Option from '@/components/atoms/Option/Option';
+import MoreButtonMain from '@/components/molecules/DropdownButton/MoreButtonMain';
 
-interface MoreButtonProps {
-  options?: string[];
-}
+const Select = Object.assign(MoreButtonMain, {
+  Option: Option,
+});
 
-const MoreButton = ({options}: MoreButtonProps) => {
-  const selectRef = useRef<HTMLDivElement>(null);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleButtonClick = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleOptionClick = () => {
-    setIsOpen(false);
-  };
-
-  useEffect(() => {
-    const handleOutside = (e: MouseEvent) => {
-      if (selectRef.current && !selectRef.current.contains(e.target as Node)) {
-        setIsOpen(false);
-      }
-    };
-
-    window.addEventListener('mousedown', handleOutside);
-    return () => window.removeEventListener('mousedown', handleOutside);
-  }, [selectRef]);
-
-  return (
-    <div ref={selectRef} className="relative">
-      <button onClick={handleButtonClick}>
-        <img src={Icon} alt="icon" />
-      </button>
-      <div className="absolute right-0 w-full top-10 min-w-fit">
-        <Dropdown isOpen={isOpen}>
-          {options &&
-            options.map((option, idx) => (
-              <Dropdown.Option
-                key={idx}
-                text={option}
-                onClick={handleOptionClick}
-              />
-            ))}
-        </Dropdown>
-      </div>
-    </div>
-  );
-};
-
-export default MoreButton;
+export default Select;
