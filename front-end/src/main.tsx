4115f0c06435aa1fc21c438db34638f503b4eff5
강혁준 <domohit5124@naver.com>
2024-08-07 09:55:32 +0900

    chore: react query devtool 추가

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import {RecoilRoot} from 'recoil';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import './index.css';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>
