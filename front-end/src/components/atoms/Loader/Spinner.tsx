381025e5d7d76199578bef3b8470c8e1efaaee38
강혁준 <domohit5124@naver.com>
2024-08-07 15:17:24 +0900

    style: Spinner 스타일 변경

--- front-end/src/components/atoms/Loader/Spinner.tsx
+++ front-end/src/components/atoms/Loader/Spinner.tsx
@@ -4,7 +4,7 @@ const Spinner = () => {
   return (
     <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center w-screen h-screen">
       <div className="spinner"></div>
-      <div className="absolute top-0 bottom-0 left-0 right-0 z-10 w-screen h-screen bg-gray-200 opacity-10"></div>
+      <div className="absolute top-0 bottom-0 left-0 right-0 z-30 w-screen h-screen bg-gray-200 opacity-20"></div>
     </div>
   );
 };
