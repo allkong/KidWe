6f7e446388e433715498ac33a6bc903eab65e7cd
강혁준 <domohit5124@naver.com>
2024-08-08 01:30:18 +0900

    feat: 서버 응답에 따른 refresh token 재발급 로직 구현

--- front-end/src/apis/axiosInstance.ts
+++ front-end/src/apis/axiosInstance.ts
@@ -1,4 +1,8 @@
-import axios, {AxiosResponse, InternalAxiosRequestConfig} from 'axios';
+import axios, {
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+  isAxiosError,
+} from 'axios';
 import {jwtToken} from '@/utils/jwtToken';
 import {getAccessToken} from '@/apis/login/getAccessToken';
 
@@ -27,22 +31,45 @@ axiosInstance.interceptors.response.use(
   },
   async error => {
     const originalRequest = error.config;
-    // access token이 없음으로 인한 오류
-    if (error.response?.status === 300 && !originalRequest._retry) {
-      // acees token 재발급 요청
-      // 만약 재발급 시 error가 발생한다면 기존 요청을 다시 보내지 않고 로그인 페이지로 redirect
+
+    // Access token이 만료된 경우
+    // Refresh token을 통해 Access token 발급 후 기존 api 재요청
+    if (
+      error.response?.status === 401 &&
+      error.response?.data.message === 'UNAUTHENTICATED_EXPIRED_TOKEN' &&
+      !originalRequest._retry
+    ) {
       const {setToken} = jwtToken();
       originalRequest._retry = true;
       try {
         const {accessToken} = await getAccessToken(axiosInstance);
         setToken(accessToken);
-        error.config.headers.Authorization = `Bearer ${accessToken}`;
+        error.config.headers.Authorization = `Bearer ${accessToken}`; // 필요 없는 코드?
         return axiosInstance(error.config);
       } catch (error) {
-        // 권한 없음 오류로 redirect를 시켜준 후 Promise chain을 끊어주는 코드
-        // window.location.href = '/';
-        return new Promise(() => {});
+        if (isAxiosError<{message: string}>(error) && error.response) {
+          const {status: errorStatus} = error;
+          const {message: errorMessage} = error.response.data;
+
+          // Refresh token 또한 만료된 경우
+          if (
+            errorStatus === 401 &&
+            errorMessage === 'UNAUTHENTICATED_EXPIRED_REFRESH_TOKEN'
+          ) {
+            // 권한 없음 오류로 redirect를 시켜준 후 Promise chain 끊어주기
+            window.location.href = '/login';
+            return new Promise(() => {});
+          }
+        }
       }
+
+      // 인가되지 않은 API에 접근한 경우
+    } else if (
+      error.response?.status === 401 &&
+      error.response?.data.message === 'UNAUTHORIZED_ACCESS'
+    ) {
+      window.location.href = '/';
+      return new Promise(() => {});
     }
     throw error;
   }
