b44806c9597a1b03f4e17628c636bfe7f6f6bca9
강혁준 <domohit5124@naver.com>
2024-08-07 10:22:32 +0900

    refactor: 일정 페이지 추가 query훅 분리

---
+++ front-end/src/hooks/schedule/useWriteKindergartenSchedule.ts
@@ -0,0 +1,46 @@
+import {postSchedule} from '@/apis/schedule/postSchedule';
+import type {PostSchedule} from '@/types/schedule/PostSchedule';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
+import {scheduleKeys} from '@/hooks/schedule/scheduleKeys';
+
+/**
+ *
+ * @param memberId
+ * @param kindergartenId query invalidate를 위해서 필요
+ * @param keyword
+ * @param content
+ * @param localDate YYYY-MM-DD
+ * @param type
+ */
+export const useWriteKindergartenSchedule = (kindergartenId: number) => {
+  const queryClient = useQueryClient();
+  const result = useMutation({
+    mutationFn: ({
+      memberId,
+      keyword,
+      content,
+      localDate,
+      type,
+    }: {
+      memberId: number;
+      keyword: string;
+      content: string;
+      localDate: string;
+      type: 'EVENT' | 'CLASS' | 'ALLNOTICE';
+    }) => {
+      const body: PostSchedule = {
+        keyword,
+        content,
+        localDate,
+        type,
+      };
+      return postSchedule(memberId, body);
+    },
+    onSuccess(_, {localDate}) {
+      queryClient.invalidateQueries({
+        queryKey: scheduleKeys.schedules(kindergartenId, localDate),
+      });
+    },
+  });
+  return result;
+};
