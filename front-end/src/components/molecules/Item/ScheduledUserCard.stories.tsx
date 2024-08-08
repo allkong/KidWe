70e48b4d9e9e2f88b830e59c3076a9362207165b
정다빈 <dbj01@naver.com>
2024-08-08 17:39:21 +0900

    feat: 예약 전송 유저 카드 컴포넌트 구현

--- front-end/src/components/molecules/Item/ScheduledUserCard.stories.tsx
+++ front-end/src/components/molecules/Item/ScheduledUserCard.stories.tsx
@@ -1,5 +1,6 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import ScheduledUserCard from '@/components/molecules/Item/ScheduledUserCard';
+import {RoleItem} from '@/enum/roleItem';
 
 const meta: Meta<typeof ScheduledUserCard> = {
   component: ScheduledUserCard,
@@ -8,6 +9,19 @@ const meta: Meta<typeof ScheduledUserCard> = {
 export default meta;
 
 type Story = StoryObj<typeof ScheduledUserCard>;
-export const Default: Story = {
-  args: {},
+export const Kindergarten: Story = {
+  args: {
+    userName: '정다빈',
+    banName: '다람쥐반',
+    writer: RoleItem.Teacher,
+    sendTime: '13:30',
+  },
+};
+
+export const Parent: Story = {
+  args: {
+    userName: '정다빈',
+    banName: '다람쥐반',
+    writer: RoleItem.Guardian,
+  },
 };
