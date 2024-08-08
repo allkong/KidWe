e348eeae33260a3c29c2e35ac49d08dcdcf4c3fe
강혁준 <domohit5124@naver.com>
2024-08-08 09:21:48 +0900

    feat: 로그인 페이지 에러 처리

--- front-end/src/pages/login/LoginMain.tsx
+++ front-end/src/pages/login/LoginMain.tsx
@@ -4,6 +4,8 @@ import Button from '@/components/atoms/Button/Button';
 import {useNavigate} from 'react-router-dom';
 import KidWe from '@/assets/kid-we.svg';
 import {useLogin} from '@/hooks/login/useLogin';
+import {toast, ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const LoginMain: React.FC = () => {
   const navigate = useNavigate();
@@ -21,58 +23,69 @@ const LoginMain: React.FC = () => {
       {email, password},
       {
         onSuccess: () => {
-          // main으로 redirect
+          navigate('/');
         },
         onError: () => {
-          // 오류창
+          toast.error('아이디와 비밀번호를 확인해주세요');
         },
       }
     );
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full min-h-screen px-10 py-6 space-y-10 bg-white">
-      <div className="flex items-center justify-center mb-7">
-        <img src={KidWe} />
-      </div>
-      <div className="w-full px-6 space-y-4">
-        <LabelInput
-          label="아이디"
-          value={email}
-          placeholder="아이디를 입력해주세요"
-          onChange={e => setEmail(e.target.value)}
-        />
-        <LabelInput
-          label="비밀번호"
-          value={password}
-          placeholder="비밀번호를 입력해주세요"
-          type="password"
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-      <div className="flex flex-col items-center justify-center w-full px-6 space-y-2">
-        <Button
-          label="로그인"
-          size="large"
-          round="full"
-          onClick={handleLoginButtonClick}
-        />
-        <div className="flex items-center justify-center text-xs text-gray-200">
-          <p>아이디 찾기</p>
-          <p className="mx-4">|</p>
-          <p>비밀번호 찾기</p>
+    <>
+      <div className="flex flex-col items-center justify-center w-full h-full min-h-screen px-10 py-6 space-y-10 bg-white">
+        <div className="flex items-center justify-center mb-7">
+          <img src={KidWe} />
         </div>
-        <div className="flex items-center justify-center gap-2 text-sm text-gray-200 cursor-default">
-          <p>아직 회원이 아니신가요?</p>
-          <p
-            onClick={handleSignUpButtonClick}
-            className="font-semibold cursor-pointer text-primary"
-          >
-            가입하기
-          </p>
+        <div className="w-full px-6 space-y-4">
+          <LabelInput
+            label="이메일"
+            value={email}
+            placeholder="이메일을 입력해주세요"
+            onChange={e => setEmail(e.target.value)}
+          />
+          <LabelInput
+            label="비밀번호"
+            value={password}
+            placeholder="비밀번호를 입력해주세요"
+            type="password"
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center w-full px-6 space-y-2">
+          <Button
+            label="로그인"
+            size="large"
+            round="full"
+            onClick={handleLoginButtonClick}
+          />
+          <div className="flex items-center justify-center text-xs text-gray-200">
+            <p>아이디 찾기</p>
+            <p className="mx-4">|</p>
+            <p>비밀번호 찾기</p>
+          </div>
+          <div className="flex items-center justify-center gap-2 text-sm text-gray-200 cursor-default">
+            <p>아직 회원이 아니신가요?</p>
+            <p
+              onClick={handleSignUpButtonClick}
+              className="font-semibold cursor-pointer text-primary"
+            >
+              가입하기
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={300}
+        hideProgressBar
+        closeOnClick
+        pauseOnFocusLoss
+        theme="light"
+        limit={1}
+      />
+    </>
   );
 };
 
