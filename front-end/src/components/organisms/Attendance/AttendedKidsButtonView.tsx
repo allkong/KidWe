5e8837839f6076d281f9c56d3c1d0cffabf9900e
강혁준 <domohit5124@naver.com>
2024-08-09 13:37:55 +0900

    feat: 출석부 화면 기능 구현

---
+++ front-end/src/components/organisms/Attendance/AttendedKidsButtonView.tsx
@@ -0,0 +1,69 @@
+import {GetAttendance} from '@/types/attendance/GetAttendance';
+import {Dayjs} from 'dayjs';
+import XSmallButton from '@/components/atoms/Button/XSmallButton';
+import Select from '@/components/molecules/DropdownButton/Select';
+import UserCardItemWithButton from '@/components/molecules/Item/UserCardItemWithButton';
+import {usePutAttendanceInfo} from '@/hooks/attendance/usePutAttendanceInfo';
+
+interface AttendedKidsButtonViewProps {
+  attendances?: GetAttendance[];
+  onClickButton?: () => void;
+  date?: Dayjs;
+}
+
+const banId = 1;
+
+const AttendedKidsButtonView = ({
+  attendances,
+  onClickButton,
+  date,
+}: AttendedKidsButtonViewProps) => {
+  const putMutate = usePutAttendanceInfo(banId);
+
+  const handleSubmit = (kidId: number) => {
+    if (date !== undefined) {
+      putMutate.mutate({
+        kidIds: [kidId],
+        year: date.get('year'),
+        month: date.get('month'),
+        day: date.get('date'),
+        attendedToday: 'NOTHING',
+      });
+    }
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between px-8 py-2 border-b border-gray-200 min-h-14">
+        <div>
+          <Select label="반 이름" size="small">
+            <Select.Option text="장미반" />
+          </Select>
+        </div>
+        <XSmallButton
+          label="선택"
+          onClick={() => {
+            onClickButton?.();
+          }}
+          variant="negative"
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center w-screen h-fit">
+        {attendances &&
+          attendances.map(attendance => (
+            <UserCardItemWithButton
+              key={attendance.attendanceId}
+              profile=""
+              userName={attendance.kidName}
+              negativeLabel="미처리"
+              onClickNegative={() => {
+                handleSubmit(attendance.kidId);
+              }}
+            />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default AttendedKidsButtonView;
--- front-end/src/components/organisms/Attendance/AttendedKidsView.tsx
+++ front-end/src/components/organisms/Attendance/AttendedKidsView.tsx
@@ -1,12 +1,145 @@
 import {Dayjs} from 'dayjs';
+import AttendedKidsSelectView from '@/components/organisms/Attendance/AttendedKidsSelectView';
+import AttendedKidsButtonView from '@/components/organisms/Attendance/AttendedKidsButtonView';
+import {useState} from 'react';
+import {GetAttendance} from '@/types/attendance/GetAttendance';
 
-interface NotAttendedKidsViewProps {
+interface AttendedKidsViewProps {
   date?: Dayjs;
 }
 
-const NotAttendedKidsView = ({date}: NotAttendedKidsViewProps) => {
-  date;
-  return <div>결석 학생들</div>;
+const data: GetAttendance[] = [
+  {
+    attendanceId: 1,
+    attendedToday: 'NOTHING',
+    banId: 1,
+    banName: '장미반',
+    date: '2024-08-08',
+    kidId: 1,
+    kidName: '강감찬',
+    reason: '',
+  },
+  {
+    attendanceId: 2,
+    attendedToday: 'ATTENDANCE',
+    banId: 1,
+    banName: '장미반',
+    date: '2024-08-08',
+    kidId: 2,
+    kidName: '이순신',
+    reason: '',
+  },
+  {
+    attendanceId: 3,
+    attendedToday: 'ABSENCE',
+    banId: 2,
+    banName: '백합반',
+    date: '2024-08-08',
+    kidId: 3,
+    kidName: '홍길동',
+    reason: '병원 방문',
+  },
+  {
+    attendanceId: 4,
+    attendedToday: 'NOTHING',
+    banId: 1,
+    banName: '장미반',
+    date: '2024-08-08',
+    kidId: 4,
+    kidName: '유관순',
+    reason: '지각',
+  },
+  {
+    attendanceId: 5,
+    attendedToday: 'NOTHING',
+    banId: 3,
+    banName: '해바라기반',
+    date: '2024-08-08',
+    kidId: 5,
+    kidName: '안중근',
+    reason: '',
+  },
+  {
+    attendanceId: 6,
+    attendedToday: 'ATTENDANCE',
+    banId: 2,
+    banName: '백합반',
+    date: '2024-08-08',
+    kidId: 6,
+    kidName: '신사임당',
+    reason: '',
+  },
+  {
+    attendanceId: 7,
+    attendedToday: 'ABSENCE',
+    banId: 1,
+    banName: '장미반',
+    date: '2024-08-08',
+    kidId: 4,
+    kidName: '나나',
+    reason: '지각',
+  },
+  {
+    attendanceId: 8,
+    attendedToday: 'ATTENDANCE',
+    banId: 1,
+    banName: '장미반',
+    date: '2024-08-08',
+    kidId: 4,
+    kidName: '뽀',
+    reason: '지각',
+  },
+];
+
+const AttendedKidsView = ({date}: AttendedKidsViewProps) => {
+  const [isShowSelect, setIsShowSelect] = useState(false);
+  const [absenceKids] = useState(
+    data.filter(
+      value =>
+        value.attendedToday === 'ABSENCE' ||
+        value.attendedToday === 'ATTENDANCE'
+    )
+  );
+
+  // const {data, refetch} = useGetAttendanceInfo(
+  //   banId,
+  //   date.get('year'),
+  //   date.get('month') + 1,
+  //   date.get('date')
+  // );
+
+  // useEffect(() => {
+  //   refetch();
+  // }, [date, refetch]);
+
+  const handleClickSelectButton = () => {
+    setIsShowSelect(false);
+  };
+
+  const handleClickButtonButton = () => {
+    setIsShowSelect(true);
+  };
+
+  return (
+    <>
+      {/* {isLoading && <Spinner />} */}
+      <div>
+        {isShowSelect ? (
+          <AttendedKidsSelectView
+            attendances={absenceKids}
+            date={date}
+            onClickButton={handleClickSelectButton}
+          />
+        ) : (
+          <AttendedKidsButtonView
+            attendances={absenceKids}
+            date={date}
+            onClickButton={handleClickButtonButton}
+          />
+        )}
+      </div>
+    </>
+  );
 };
 
-export default NotAttendedKidsView;
+export default AttendedKidsView;
