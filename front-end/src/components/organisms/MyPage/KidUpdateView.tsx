e5e203ff45f2cceeb98d96c78f3a9f1619463a2b
강혁준 <domohit5124@naver.com>
2024-08-11 16:46:33 +0900

    fix: 아이 정보 변경 페이지 빌드 오류 수정

--- front-end/src/components/organisms/MyPage/KidUpdateView.tsx
+++ front-end/src/components/organisms/MyPage/KidUpdateView.tsx
@@ -3,12 +3,20 @@ import CheckBoxButton from '@/components/atoms/CheckBox/CheckBoxButton';
 import CalendarButton from '@/components/molecules/Button/CalendarButton';
 import AllergyView from '@/components/organisms/Food/AllergyView';
 import {ALLERGIES} from '@/constants/allergy';
+// import ImageUploadButton from '@/components/molecules/Button/ImageUploadButton';
+import XSmallButton from '@/components/atoms/Button/XSmallButton';
 
 const KidUpdateView = () => {
   return (
-    <div className="flex-col items-end w-full pt-10 space-y-5 text-gray-300">
+    <div className="flex-col items-end w-full pt-10 text-gray-300">
+      <div className="flex flex-col items-center justify-center w-full gap-3 py-3">
+        {/* <ImageUploadButton userPicture="" onChangePicture={() => {}} /> */}
+        <div className="w-fit">
+          <XSmallButton label="삭제" />
+        </div>
+      </div>
       <LabelInput label="자녀 이름" value="" placeholder="자녀 이름" />
-      <div className="flex items-end justify-between gap-9 h-fit">
+      <div className="flex items-end justify-between my-4 gap-9 h-fit">
         <div className="flex-grow">
           <CalendarButton
             position="right"
@@ -23,6 +31,7 @@ const KidUpdateView = () => {
           </div>
         </div>
       </div>
+      <p>알레르기 정보</p>
       <AllergyView datas={ALLERGIES} />
     </div>
   );
