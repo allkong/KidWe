2451afbb0d54bdc7e44f8ddec4f45cdc540ba63e
강혁준 <domohit5124@naver.com>
2024-08-11 16:33:41 +0900

    feat: 이미지 등록 컴포넌트 구현

---
+++ front-end/src/components/molecules/Button/ImageUploadButton.tsx
@@ -0,0 +1,52 @@
+import ProfileImage from '@/components/atoms/Image/ProfileImage';
+import NoProfile from '@/assets/no-profile.png';
+import {ChangeEvent, useRef} from 'react';
+import {getImageFromInputEvent} from '@/utils/getImageFromInputEvent';
+
+interface ImageUploadButtonProps {
+  userPicture?: string;
+  onChangeFile?: (image: File) => void;
+  onChangePreview?: (image: string) => void;
+}
+
+const ImageUploadButton = ({
+  userPicture,
+  onChangeFile,
+  onChangePreview,
+}: ImageUploadButtonProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageChangeClick = () => {
+    inputRef.current?.click();
+  };
+
+  const handleImagePreview = async (e: ChangeEvent<HTMLInputElement>) => {
+    const {file, preview} = await getImageFromInputEvent(e);
+    if (file !== null && preview !== null) {
+      onChangeFile?.(file);
+      onChangePreview?.(preview);
+    }
+  };
+
+  return (
+    <div onClick={handleImageChangeClick} className="relative w-fit h-fit">
+      <input
+        ref={inputRef}
+        id="image"
+        type="file"
+        accept="image/*"
+        onChange={handleImagePreview}
+        className="hidden"
+      />
+      <div className="absolute flex items-center justify-center w-20 h-20 text-2xl font-bold transition-opacity bg-gray-200 rounded-full opacity-0 hover:opacity-30">
+        +
+      </div>
+      <ProfileImage
+        src={userPicture && userPicture !== '' ? userPicture : NoProfile}
+        size="5rem"
+      />
+    </div>
+  );
+};
+
+export default ImageUploadButton;
