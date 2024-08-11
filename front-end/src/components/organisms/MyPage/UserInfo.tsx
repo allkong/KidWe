dee42de8b4dd1414e24021fa3a774a5137715a18
강혁준 <domohit5124@naver.com>
2024-08-11 16:36:26 +0900

    style: 유저 정보 페이지 디자인 수정

--- front-end/src/components/organisms/MyPage/UserInfo.tsx
+++ front-end/src/components/organisms/MyPage/UserInfo.tsx
@@ -1,3 +1,6 @@
+import NoProfile from '@/assets/no-profile.png';
+import ProfileImage from '@/components/atoms/Image/ProfileImage';
+
 interface UserInfoProps {
   name: string;
   role: string;
@@ -6,10 +9,13 @@ interface UserInfoProps {
 
 const UserInfo = ({name, role, banName}: UserInfoProps) => {
   return (
-    <div className="flex flex-col w-full px-10 py-10 text-gray-300 bg-white">
-      <p className="text-3xl font-semibold">{name}</p>
-      <p>{role}</p>
-      <p>{banName}</p>
+    <div className="flex flex-row justify-between w-full px-10 py-10 text-gray-300 bg-white">
+      <div>
+        <p className="text-3xl font-semibold">{name}</p>
+        <p>{role}</p>
+        <p>{banName}</p>
+      </div>
+      <ProfileImage src={NoProfile} size="6rem" />
     </div>
   );
 };
