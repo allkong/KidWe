09eb9bd1f607a187715636161a63fa3adc63afa5
박동환 <znxn123@naver.com>
2024-08-12 13:10:01 +0900

    fix: 임시 조치

--- back-end/common/src/main/java/yeomeong/common/security/JwtAuthenticationFilter.java
+++ back-end/common/src/main/java/yeomeong/common/security/JwtAuthenticationFilter.java
@@ -35,66 +35,80 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,FilterChain filterChain)
         throws ServletException, IOException, ExpiredJwtException {
-        if (request.getRequestURI().equals("/login")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
-        try {
-            log.info("[JwtAuthenticationFilter start] {}", request.getRequestURI());
-            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-
-            if (authorizationHeader == null) {
-                log.info("[JwtAuthenticationFilter] Authorization header is null");
-                throw new CustomException(ErrorCode.TOKEN_MISSING);
-            }
-
-            if (!authorizationHeader.startsWith("Bearer ")) {
-                log.info("[JwtAuthenticationFilter] Authorization header is not starting with Bearer");
-                throw new CustomException(ErrorCode.TOKEN_NOT_BEARER);
-            }
-
-            if (jwtService.isTokenStored(authorizationHeader) && !request.getRequestURI().equals("/refresh")) {
-                log.info("[JwtAuthenticationFilter] This token is refresh token");
-                throw new CustomException(ErrorCode.REFRESH_TOKEN_NOT_ALLOWED);
-            }
-
-            if (!jwtService.isTokenStored(authorizationHeader) && request.getRequestURI().equals("/refresh")) {
-                log.info("[JwtAuthenticationFilter] This token is access token");
-                throw new CustomException(ErrorCode.ACCESS_TOKEN_REQUIRED);
-            }
-
-            if (jwtService.isLogoutAccessToken(authorizationHeader)) {
-                log.info("[JwtAuthenticationFilter] Logout access token");
-                throw new CustomException(ErrorCode.LOGGED_OUT_ACCESS_TOKEN);
-            }
-
-            if (JwtUtil.isExpired(authorizationHeader)) {
-                log.info("[JwtAuthenticationFilter] Token is expired");
-                throw new CustomException(ErrorCode.EXPIRED_TOKEN);
-            }
-
-            log.info("[JwtAuthenticationFilter] Token is valid");
-            Member loginMember = memberService.getMemberByEmail(
-                JwtUtil.getLoginEmail(authorizationHeader));
-
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                userDetailsService.loadUserByUsername(loginMember.getEmail()),
-                loginMember.getPassword(),
-                List.of(new SimpleGrantedAuthority(loginMember.getRole().toString())));
-            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            filterChain.doFilter(request, response);
-
-        } catch (CustomException e) {
-            request.setAttribute("exception", e.getErrorCode());
-            throw new CustomException(e.getErrorCode());
-        } catch (JwtException e) {
-            throw new CustomException(ErrorCode.UNKNOWN_TOKEN_ERROR);
-        }
-
+//        if(request.getRequestURI().equals("/swagger-ui.html") ||
+//            request.getRequestURI().equals("/swagger-ui/index.html") ||
+//            request.getRequestURI().equals("/swagger-ui/**") ||
+//            request.getRequestURI().equals("/api-docs") ||
+//            request.getRequestURI().equals("/api-docs/*") ||
+//            request.getRequestURI().equals("/v3/api-docs/*") ||
+//            request.getRequestURI().equals("/swagger-ui/swagger-initializer.js")
+//        ) {
+//            filterChain.doFilter(request, response);
+//            return;
+//        }
         filterChain.doFilter(request, response);
+        return;
+
+//        if (request.getRequestURI().equals("/login")) {
+//            filterChain.doFilter(request, response);
+//            return;
+//        }
+//
+//        try {
+//            log.info("[JwtAuthenticationFilter start] {}", request.getRequestURI());
+//            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+//
+//            if (authorizationHeader == null) {
+//                log.info("[JwtAuthenticationFilter] Authorization header is null");
+//                throw new CustomException(ErrorCode.TOKEN_MISSING);
+//            }
+//
+//            if (!authorizationHeader.startsWith("Bearer ")) {
+//                log.info("[JwtAuthenticationFilter] Authorization header is not starting with Bearer");
+//                throw new CustomException(ErrorCode.TOKEN_NOT_BEARER);
+//            }
+//
+//            if (jwtService.isTokenStored(authorizationHeader) && !request.getRequestURI().equals("/refresh")) {
+//                log.info("[JwtAuthenticationFilter] This token is refresh token");
+//                throw new CustomException(ErrorCode.REFRESH_TOKEN_NOT_ALLOWED);
+//            }
+//
+//            if (!jwtService.isTokenStored(authorizationHeader) && request.getRequestURI().equals("/refresh")) {
+//                log.info("[JwtAuthenticationFilter] This token is access token");
+//                throw new CustomException(ErrorCode.ACCESS_TOKEN_REQUIRED);
+//            }
+//
+//            if (jwtService.isLogoutAccessToken(authorizationHeader)) {
+//                log.info("[JwtAuthenticationFilter] Logout access token");
+//                throw new CustomException(ErrorCode.LOGGED_OUT_ACCESS_TOKEN);
+//            }
+//
+//            if (JwtUtil.isExpired(authorizationHeader)) {
+//                log.info("[JwtAuthenticationFilter] Token is expired");
+//                throw new CustomException(ErrorCode.EXPIRED_TOKEN);
+//            }
+//
+//            log.info("[JwtAuthenticationFilter] Token is valid");
+//            Member loginMember = memberService.getMemberByEmail(
+//                JwtUtil.getLoginEmail(authorizationHeader));
+//
+//            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+//                userDetailsService.loadUserByUsername(loginMember.getEmail()),
+//                loginMember.getPassword(),
+//                List.of(new SimpleGrantedAuthority(loginMember.getRole().toString())));
+//            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//
+//            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+//            filterChain.doFilter(request, response);
+//
+//        } catch (CustomException e) {
+//            request.setAttribute("exception", e.getErrorCode());
+//            throw new CustomException(e.getErrorCode());
+//        } catch (JwtException e) {
+//            throw new CustomException(ErrorCode.UNKNOWN_TOKEN_ERROR);
+//        }
+//
+//        filterChain.doFilter(request, response);
     }
 
 }
