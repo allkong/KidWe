688374ccf3d8c68587696b103845e9a8436982d6
강혁준 <domohit5124@naver.com>
2024-08-12 14:43:43 +0900

    fix: UserData 인터페이스 수정

--- front-end/src/utils/user-data-implements/UserDataInterface.ts
+++ front-end/src/utils/user-data-implements/UserDataInterface.ts
@@ -6,8 +6,12 @@ export interface UserDataInterface {
   getUserData: () => UserData | null;
   getMemberId: () => number | null;
   getMemberEmail: () => string | null;
-  getMemberRole: () => string | null;
-  getMemberStatus: () => string | null;
+  getMemberRole: () =>
+    | 'ROLE_DIRECTOR'
+    | 'ROLE_GUARDIAN'
+    | 'ROLE_TEACHER'
+    | null;
+  getMemberStatus: () => 'NOTHING' | 'DECLINE' | 'PENDING' | 'ACCEPT' | null;
   getKindergartenId: () => number | null;
   getBanId: () => number | null;
   getKidIds: () => number[] | null;
