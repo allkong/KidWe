b569fd146c881a24c49757fc48513217c87fc4be
강혁준 <domohit5124@naver.com>
2024-08-12 16:51:46 +0900

    fix: MoreButton 컴포넌트 안닫히는 오류 수정

--- front-end/src/components/molecules/DropdownButton/MoreButtonMain.tsx
+++ front-end/src/components/molecules/DropdownButton/MoreButtonMain.tsx
@@ -1,12 +1,33 @@
 import Icon from '@/assets/icons/more-line.svg';
 import Dropdown from '@/components/atoms/Dropdown/Dropdown';
+import {OptionProps} from '@/components/atoms/Option/Option';
 import {useClickOutside} from '@/hooks/useClickOutside';
-import React, {useRef} from 'react';
+import React, {isValidElement, useRef} from 'react';
 
 export interface MoreButtonProps {
   children?: React.ReactNode;
   position?: 'left' | 'right';
   align?: 'vertical' | 'horizontal';
+}
+
+function addEventListenerToChild(
+  children: React.ReactNode,
+  handleItemClick: () => void
+) {
+  const childrenArray = React.Children.toArray(children);
+  return childrenArray
+    .filter(child => isValidElement(child))
+    .map(child => {
+      if (isValidElement<OptionProps>(child)) {
+        return React.cloneElement(child, {
+          onClick: () => {
+            child.props?.onClick?.();
+            handleItemClick();
+          },
+        });
+      }
+      return child;
+    });
 }
 
 const MoreButton = ({
@@ -21,8 +42,14 @@ const MoreButton = ({
     setIsOpen(!isOpen);
   };
 
+  const handleItemClick = () => {
+    setIsOpen(false);
+  };
+
   const positionClass = position === 'left' ? 'right-0' : 'left-0';
   const alignClass = align === 'horizontal' ? '' : 'rotate-90';
+
+  const addedChilds = addEventListenerToChild(children, handleItemClick);
 
   return (
     <div ref={selectRef} className="relative flex items-center w-fit h-fit">
@@ -30,7 +57,7 @@ const MoreButton = ({
         <img className={`${alignClass}`} src={Icon} alt="icon" />
       </button>
       <div className={`${positionClass} absolute w-full top-10 min-w-fit`}>
-        <Dropdown isOpen={isOpen}>{children}</Dropdown>
+        <Dropdown isOpen={isOpen}>{addedChilds}</Dropdown>
       </div>
     </div>
   );
