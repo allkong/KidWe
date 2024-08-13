8b451310e93b672d13265c51562f101ad6be1034
galler-ist <swghj@naver.com>
2024-08-13 21:37:15 +0900

    feat : 반 관리 component 구현

--- front-end/src/pages/kindergarten-management/banManagement.tsx
+++ front-end/src/pages/kindergarten-management/banManagement.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/organisms/Navigation/Header';
 import NavigationBar from '@/components/organisms/Navigation/NavigationBar';
 import {containerNavigatorClass} from '@/styles/styles';
 import Button from '@/components/atoms/Button/Button';
-
+import BanCardItem from '@/components/molecules/Item/BanCardItem';
 const BanManagement = () => {
   const handleAddBan = () => {};
   return (
@@ -11,8 +11,35 @@ const BanManagement = () => {
     >
       <Header title="반 관리" buttonType="back" />
       <div>
-        <p>반 이름 + 원생 몇 명 + 교사 몇 명 + : (수정할게있나)</p>
-        <Button label="+ 반 추가하기" onClick={handleAddBan}></Button>
+        <BanCardItem
+          cardType="detail"
+          options={[
+            {
+              text: '반 수정',
+              onClick: () => {
+                console.log('반 수정');
+              },
+            },
+            {
+              text: '반 삭제',
+              onClick: () => {
+                console.log('반 삭제');
+                console.log('이건 반에 원생, 교사가 없을 때만 표시됨');
+              },
+            },
+          ]}
+          banName="치타반"
+          kidCount={123}
+          teacherCount={12}
+        />
+        <p>이제 이 반 관련을 props로 받게하기! + api </p>
+      </div>
+      <div
+        className={
+          'w-full flex justify-center items-center absolute bottom-32 left-1/2 transform -translate-x-1/2'
+        }
+      >
+        <Button label="회원 가입" onClick={handleAddBan} />
       </div>
       <NavigationBar />
     </div>
