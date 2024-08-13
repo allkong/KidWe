ed7669da6fe1c14b45af7eadc411d260d6381978
강혁준 <domohit5124@naver.com>
2024-08-13 10:33:35 +0900

    fix: api url 수정

--- front-end/src/apis/my-page/patchUserInfo.ts
+++ front-end/src/apis/my-page/patchUserInfo.ts
@@ -1,6 +1,6 @@
 import axiosInstance from '@/apis/axiosInstance';
 
 export const patchUserInfo = async (body: FormData) => {
-  const result = await axiosInstance.patch(`profile`, body);
+  const result = await axiosInstance.patch(`members/profile`, body);
   return result.data;
 };
