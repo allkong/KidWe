eb127bc85e645b0b79d239c4602c8744cdebfb4a
강혁준 <domohit5124@naver.com>
2024-08-13 13:56:12 +0900

    feat: 새로고침 시 토큰 유지 로직

--- front-end/src/hooks/useApiHandler.ts
+++ front-end/src/hooks/useApiHandler.ts
@@ -13,8 +13,6 @@ const useApiHandler = () => {
       const {message, code} = error.response.data;
 
       if (status === 401 && code === 'UNAUTHENTICATED_EXPIRED_REFRESH_TOKEN') {
-        console.log('만료');
-
         deleteAccessToken();
         deleteRefreshToken();
         deleteUserData();
@@ -23,22 +21,26 @@ const useApiHandler = () => {
       } else if (message) {
         toast.error(message);
       } else {
-        if (status === 400) {
-          toast.error('올바르지 않은 입력입니다.');
-        } else if (status === 401) {
-          toast.error('권한이 없습니다.');
-        } else if (status === 404) {
-          toast.error('존재하지 않는 요청입니다.');
-        } else if (status === 500) {
-          toast.error('문제가 발생했습니다.\n잠시 후 다시 시도해주세요.');
-        } else {
-          toast.error('에러가 발생했습니다.\n관리자에게 문의해주세요.');
-        }
+        defaultMessage(status);
       }
     }
   }, []);
 
   return {errorHandler};
+};
+
+const defaultMessage = (status: number) => {
+  if (status === 400) {
+    toast.error('올바르지 않은 입력입니다.');
+  } else if (status === 401) {
+    toast.error('권한이 없습니다.');
+  } else if (status === 404) {
+    toast.error('존재하지 않는 요청입니다.');
+  } else if (status === 500) {
+    toast.error('문제가 발생했습니다.\n잠시 후 다시 시도해주세요.');
+  } else {
+    toast.error('에러가 발생했습니다.\n관리자에게 문의해주세요.');
+  }
 };
 
 export default useApiHandler;
