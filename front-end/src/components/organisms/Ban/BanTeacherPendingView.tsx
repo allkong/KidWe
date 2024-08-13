95bd7f3021cd55439e9a8455bd6d0ae2d3e07556
galler-ist <swghj@naver.com>
2024-08-13 14:34:46 +0900

    feat : 반관리 - 교사관리 axios 구현
    -organism/Ban/BanTeacherAcceptView, PendingView
    getTeacherAccept, getTeacherPending, putTeacherAccept
    Accept & TeahcerInfo type 구현
    (현재 pending 상태에 맞는 유치원 원장님 데이터가 없어 테스트가 힘듦)

--- front-end/src/components/organisms/Ban/BanTeacherPendingView.tsx
+++ front-end/src/components/organisms/Ban/BanTeacherPendingView.tsx
@@ -1,13 +1,99 @@
-import UserCardItem from '@/components/molecules/Item/UserCardItem';
-
+import UserCardItemWithButton from '@/components/molecules/Item/UserCardItemWithButton';
+import {useState, useEffect} from 'react';
+import Modal from '@/components/organisms/Modal/Modal';
+import ModalPortal from '@/components/organisms/Modal/ModalPortal';
+import {getTeacherPending} from '@/apis/management/getTeacherPending';
+import {putTeacherAccept} from '@/apis/management/putTeacherAccept';
+import {TeacherInfo} from '@/types/management/TeacherInfo';
+import {getKindergartenId} from '@/utils/userData';
 const BanTeacherPendingView = () => {
+  const [teacherAcceptList, setTeacherAcceptList] = useState<TeacherInfo[]>([]);
+  // const [kindergartenId, setKindergartenId] = useState(getKindergartenId());
+  const [kindergartenId, setKindergartenId] = useState(getKindergartenId);
+  // const navigate = useNavigate();
+  const [isNegativeModalOpen, setIsNegativeModalOpen] = useState(false);
+  const handleAcceptTeacher = async (id: number) => {
+    try {
+      await putTeacherAccept({id, accepted: true});
+      if (kindergartenId) {
+        const updatedList = await getTeacherPending(kindergartenId);
+        setTeacherAcceptList(updatedList);
+      }
+    } catch (error) {
+      console.error('Failed to accept teacher', error);
+    }
+  };
+  const handleOpenNegativeModal = () => {
+    setIsNegativeModalOpen(true);
+  };
+
+  const handleCloseNegativeModal = () => {
+    setIsNegativeModalOpen(false);
+  };
+
+  useEffect(() => {
+    const fetchTeacherAcceptList = async () => {
+      try {
+        if (kindergartenId) {
+          const response = await getTeacherPending(kindergartenId);
+          setTeacherAcceptList(response);
+        }
+      } catch (error) {
+        console.error('Failed to featch teacher pending list', error);
+      }
+    };
+    fetchTeacherAcceptList();
+  }, [kindergartenId]);
+
   return (
     <div>
-      <UserCardItem
-        cardType="detail"
-        profile=""
-        userName="김교사"
-      ></UserCardItem>
+      {teacherAcceptList.map(teacher => (
+        <UserCardItemWithButton
+          key={teacher.memberId}
+          profile=""
+          userName={teacher.name}
+          negativeLabel="거절"
+          positiveLabel="수락"
+          onClickNegative={() => {
+            handleOpenNegativeModal();
+          }}
+          onClickPositive={() => {
+            handleAcceptTeacher(teacher.memberId);
+          }}
+        />
+      ))}
+      <p>거절은 누르면 modal 띄우기!(완료)</p>
+      <p>수락 누르면 teacher의 상태 변경!</p>
+      <ModalPortal>
+        <Modal isOpen={isNegativeModalOpen}>
+          <Modal.Header title="알림" />
+          <Modal.Body>
+            <div className="flex flex-col items-center justify-center py-10 h-44">
+              <p>대기 중인 교사를 거절하시겠습니까?</p>
+              <p>
+                거절한 요청은 복구할 수 <b>없습니다</b>
+              </p>
+            </div>
+          </Modal.Body>
+          <Modal.BottomButton
+            label="취소"
+            onClick={handleCloseNegativeModal}
+            round="full"
+            size="large"
+            variant="negative"
+          ></Modal.BottomButton>
+          <Modal.BottomButton
+            label="거절"
+            onClick={handleCloseNegativeModal}
+            round="full"
+            size="large"
+            variant="positive"
+          ></Modal.BottomButton>
+          <Modal.Background
+            onClick={handleCloseNegativeModal}
+          ></Modal.Background>
+        </Modal>
+      </ModalPortal>
     </div>
   );
 };
