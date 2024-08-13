8c3c6cfc659f0f94f03857204c9236be2ea5d486
정다빈 <dbj01@naver.com>
2024-08-13 13:22:57 +0900

    fix: login 요청 path 수정

--- front-end/src/apis/login/login.ts
+++ front-end/src/apis/login/login.ts
@@ -5,6 +5,6 @@ export const login = async (
   email: string,
   password: string
 ): Promise<LoginResponse> => {
-  const response = await axiosInstance.post('/auth/login', {email, password});
+  const response = await axiosInstance.post('/login', {email, password});
   return response.data;
 };
