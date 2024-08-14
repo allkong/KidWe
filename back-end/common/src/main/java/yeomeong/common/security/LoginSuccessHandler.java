e94bd8c35b65bd1d91e05e589130536c193dde74
박동환 <znxn123@naver.com>
2024-08-14 16:00:40 +0900

    feat: login success dto ban id 추가

--- back-end/common/src/main/java/yeomeong/common/security/LoginSuccessHandler.java
+++ back-end/common/src/main/java/yeomeong/common/security/LoginSuccessHandler.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -69,6 +68,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
                 Kid kid = kidMember.getKid();
                 loginResponseDto.setKidId(kid.getId());
                 loginResponseDto.setKindergartenId(kid.getKindergarten().getId());
+                loginResponseDto.setBanId(kid.getBan().getId());
             }
         }
 
