9aebed80ff13100622dc2c8a693f43a5a240159b
강혁준 <domohit5124@naver.com>
2024-08-15 01:57:38 +0900

    feat: 원장님 알림장 조회 권한 구현

--- front-end/src/pages/daily-note/DailyNoteListView.tsx
+++ front-end/src/pages/daily-note/DailyNoteListView.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import dayjs from 'dayjs';
 
@@ -15,24 +15,30 @@ import MonthDivider from '@/components/atoms/Divider/MonthDivider';
 import ScheduledUserCard from '@/components/molecules/Item/ScheduledUserCard';
 import WriteButton from '@/components/atoms/Button/WriteButton';
 import NavigationBar from '@/components/organisms/Navigation/NavigationBar';
+import {isDirector} from '@/utils/auth/isDirector';
+import DirectorSelectItem from '@/components/organisms/Medication/DirectorSelectItem';
 
 const DailyNoteListView = () => {
   const navigate = useNavigate();
   const [currentMonth, setCurrentMonth] = useState(dayjs().startOf('month'));
 
   const memberRole = getMemberRole() as RoleItem;
-  let id: number | null = 0;
-  if (memberRole === RoleItem.Guardian) {
-    id = getKidId();
-  } else if (memberRole === RoleItem.Teacher) {
-    id = getBanId();
-  } else if (memberRole === RoleItem.Director) {
-    // 선택한 반 id로 변경
-    id = 1;
-  }
+
+  const [id, setId] = useState<number | null>(null);
+  const handleIdChange = (value: number) => {
+    setId(value);
+  };
+
+  useEffect(() => {
+    if (memberRole === RoleItem.Guardian) {
+      setId(getKidId());
+    } else if (memberRole === RoleItem.Teacher) {
+      setId(getBanId());
+    }
+  }, [memberRole]);
 
   const {data} = useDailyNoteList(
-    id!,
+    id,
     getMemberId()!,
     currentMonth.year(),
     currentMonth.month() + 1,
@@ -67,10 +73,15 @@ const DailyNoteListView = () => {
         onClickLeft={handleLeftClick}
         onClickRight={handleRightClick}
       />
-      <div className={`${containerNavigatorClass} pt-[6.5rem]`}>
-        {data &&
-        Object.keys(data.dailyNoteListItemResponseDtos).length === 0 ? (
-          <div className="flex items-center justify-center h-full">
+      <div className={`${containerNavigatorClass} pt-[6.5rem] flex flex-col`}>
+        <DirectorSelectItem
+          memberRole={memberRole}
+          onBanChange={handleIdChange}
+        />
+        {(data &&
+          Object.keys(data.dailyNoteListItemResponseDtos).length === 0) ||
+        !id ? (
+          <div className="flex items-center justify-center flex-grow h-full">
             <NoResult text="등록된 알림장이 없어요" />
           </div>
         ) : (
@@ -99,7 +110,7 @@ const DailyNoteListView = () => {
           ))
         )}
       </div>
-      <WriteButton onClick={handleWriteButtonClick} />
+      {!isDirector() && <WriteButton onClick={handleWriteButtonClick} />}
       <NavigationBar />
     </div>
   );
