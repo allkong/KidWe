9ed49a7103154821a0fa2c485b4b9182194c3278
강혁준 <domohit5124@naver.com>
2024-08-14 09:46:33 +0900

    style: 마이 페이지 디자인 수정

--- front-end/src/components/organisms/MyPage/UserInfo.tsx
+++ front-end/src/components/organisms/MyPage/UserInfo.tsx
@@ -2,17 +2,19 @@ import NoProfile from '@/assets/no-profile.png';
 import ProfileImage from '@/components/atoms/Image/ProfileImage';
 import {useGetUserInfo} from '@/hooks/my-page/useGetUserInfo';
 import {getMemberId} from '@/utils/userData';
+import {ROLE_NAMES} from '@/constants/roleNames';
+import {RoleItem} from '@/enum/roleItem';
 
 const UserInfo = () => {
   const {data} = useGetUserInfo(getMemberId()!);
 
   return (
-    <div className="flex flex-row justify-between w-full px-10 py-10 mb-2 text-gray-300 bg-white">
+    <div className="flex flex-row items-center justify-between w-full px-10 py-10 mb-2 text-gray-300 bg-white">
       <div>
         <p className="text-3xl font-semibold">{data?.name}</p>
-        <p>{data?.role}</p>
+        <p>{ROLE_NAMES[data?.role as RoleItem]}</p>
       </div>
-      <ProfileImage src={NoProfile} size="5rem" />
+      <ProfileImage src={NoProfile} size="7rem" />
     </div>
   );
 };
