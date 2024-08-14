b9adb6e519d3bec2587ba19151277e1a7a306a89
강혁준 <domohit5124@naver.com>
2024-08-14 15:23:07 +0900

    feat: 권한에 따른 홈 화면 메뉴 설정

--- front-end/src/components/organisms/Content/HomeMenu.tsx
+++ front-end/src/components/organisms/Content/HomeMenu.tsx
@@ -1,13 +1,15 @@
 import {Link} from 'react-router-dom';
-import {MENU_ITEMS} from '@/constants/home/menuItems';
 import MainMenu from '@/components/atoms/Menu/MainMenu';
+import {getMenuItem} from '@/utils/getMenuItem';
+import {getMemberRole} from '@/utils/userData';
 
 const HomeMenu = () => {
+  const menus = getMenuItem(getMemberRole()!);
+
   return (
     <div className="flex justify-center bg-white rounded-lg">
       <div className="grid grid-cols-4 p-7 gap-x-7 gap-y-5">
-        {/* 역할에 따라 보여주는 메뉴가 달라야 함 */}
-        {MENU_ITEMS.map(item => (
+        {menus.map(item => (
           <Link key={item.id} to={item.to}>
             <MainMenu key={item.id} img={item.img} text={item.text} />
           </Link>
---
+++ front-end/src/utils/getMenuItem.ts
@@ -0,0 +1,30 @@
+import {MENU_ITEMS} from '@/constants/home/menuItems';
+
+interface MenuItem {
+  id: number;
+  img: string;
+  text: string;
+  to: string;
+}
+
+const directorMenus = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const teacherMenus = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const guardianMenus = [1, 2, 5, 6, 7, 10];
+
+export const getMenuItem = (
+  role: 'ROLE_DIRECTOR' | 'ROLE_TEACHER' | 'ROLE_GUARDIAN'
+) => {
+  if (role === 'ROLE_DIRECTOR') {
+    return MENU_ITEMS.filter((menu: MenuItem) =>
+      directorMenus.includes(menu.id)
+    );
+  } else if (role === 'ROLE_TEACHER') {
+    return MENU_ITEMS.filter((menu: MenuItem) =>
+      teacherMenus.includes(menu.id)
+    );
+  } else {
+    return MENU_ITEMS.filter((menu: MenuItem) =>
+      guardianMenus.includes(menu.id)
+    );
+  }
+};
